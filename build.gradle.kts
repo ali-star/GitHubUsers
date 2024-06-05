155cd0eca812f1a370b7a0b1a9fdbcc2df6e334b
Ali Mohseni Rad <obanstar1@gmail.com>
2024-06-05 21:48:35 +0200

    Migrate to kotlin 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,5 +8,6 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.hilt) apply false
 }
